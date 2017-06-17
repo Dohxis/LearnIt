aa2e5f608be756f299de51c1a4e0c98f848ccd4c
Ainoras <ainoraz@gmail.com>
2017-06-17 14:31:01 +0300

    Lessons page carousel functionallity

--- app/pages/Lessons.js
+++ app/pages/Lessons.js
@@ -3,15 +3,242 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
 export default class Lessons extends Component {
-	render(){
-		return(
-			<View>
-				<Text> Lesson </Text>
-			</View>
-		)
-	}
+	render () {
+		return (
+			<Carousel
+			sliderWidth={sliderWidth}
+			itemWidth={itemWidth}
+			firstItem={0}
+			inactiveSlideScale={0.94}
+			inactiveSlideOpacity={0.6}
+			enableMomentum={false}
+			containerCustomStyle={styles.slider}
+			contentContainerCustomStyle={styles.sliderContainer}
+			showsHorizontalScrollIndicator={false}
+			snapOnAndroid={true}
+			removeClippedSubviews={false}
+			>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+			activeOpacity={0.7}
+			style={styles.slideInnerContainer}
+			onPress={() => { alert(`You've clicked`); }}
+			>
+				<View>
+					<View style={styles.imageContainer}>
+					<Image
+					source={require('../components/img/two.png')}
+					style={styles.image}
+					/>
+					<View style={styles.radiusMask} />
+					</View>
+					<View style={styles.textContainer}>
+					<Text style={styles.title}> HELLO </Text>
+					<Text style={styles.subtitle} numberOfLines={2}>This is a thing</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+		   </Carousel>
+       );
+   }
 }
+
+const horizontalMargin = 20;
+const slideWidth = 280;
+
+function wp (percentage) {
+    const value = (percentage * Dimensions.get('window').width) / 100;
+    return Math.round(value);
+}
+
+const sliderWidth = Dimensions.get('window').width;
+const itemWidth = slideWidth;
+const itemHeight = 200;
+const slideHeight = 400;
+const win = Dimensions.get('window');
+const itemHorizontalMargin = wp(2);
+const entryBorderRadius = 0;
+
+const colors = {
+    black: '#1a1917',
+    gray: '#888888',
+    background1: 'hsl(15, 55%, 50%)',
+    background2: 'hsl(230, 30%, 45%)'
+};
+
+const styles = StyleSheet.create({
+	    slideInnerContainer: {
+	        width: itemWidth,
+	        height: 500,
+	        paddingHorizontal: itemHorizontalMargin,
+	        paddingBottom: 0 // needed for shadow
+	    },
+	    imageContainer: {
+	        flex: 0,
+	        backgroundColor: 'white',
+	        borderTopLeftRadius: entryBorderRadius,
+	        borderTopRightRadius: entryBorderRadius
+	    },
+	    imageContainerEven: {
+	        backgroundColor: colors.black
+	    },
+	    image: {
+	        resizeMode: 'cover',
+	        borderRadius: entryBorderRadius,
+	        borderTopLeftRadius: entryBorderRadius,
+	        borderTopRightRadius: entryBorderRadius,
+			width: itemWidth,
+	  	  	height: 400,
+	    },
+	    // image's border radius is buggy on ios; let's hack it!
+	    radiusMask: {
+	        position: 'absolute',
+	        bottom: 0,
+	        left: 0,
+	        right: 0,
+	        height: entryBorderRadius,
+	        backgroundColor: 'white'
+	    },
+	    radiusMaskEven: {
+	        backgroundColor: colors.black
+	    },
+	    textContainer: {
+	        justifyContent: 'center',
+	        paddingTop: 20 - entryBorderRadius,
+	        paddingBottom: 20,
+	        paddingHorizontal: 16,
+	        backgroundColor: 'white',
+	        borderBottomLeftRadius: entryBorderRadius,
+	        borderBottomRightRadius: entryBorderRadius
+	    },
+	    textContainerEven: {
+	        backgroundColor: colors.black
+	    },
+	    title: {
+	        color: colors.black,
+	        fontSize: 13,
+	        fontWeight: 'bold',
+	        letterSpacing: 0.5
+	    },
+	    titleEven: {
+	        color: 'white'
+	    },
+	    subtitle: {
+	        marginTop: 6,
+	        color: colors.gray,
+	        fontSize: 12,
+	        fontStyle: 'italic'
+	    },
+	    subtitleEven: {
+	        color: 'rgba(255, 255, 255, 0.7)'
+	    }
+});
